3f5d191255c03139b7b5ed99b3879f5c0c7f034b
venkat <naresh3456@gmail.com>
2023-12-24 17:20:49 -0500

    7 and 8

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -91,5 +91,9 @@ public class ArithmeticException {
         System.out.println("Remote changes 11111111111111111111111");
          System.out.println("Remote changes 2222222222222222222222");
 
+         System.out.println("local changes +++++++++++++++++++++++++");
+         System.out.println("local changes --------------------------");
+         System.out.println("local changes 77777777777777777777777777");
+         System.out.println("local changes 888888888888888888888888888");
     }
 }
