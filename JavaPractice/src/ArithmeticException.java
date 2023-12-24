1758daab27e1e616c99bac3ddd04d1d98e349edb
venkat <naresh3456@gmail.com>
2023-12-24 13:50:06 -0500

    Revert "Added  tenth line"
    This reverts commit c6dabfabb9ffa232dc66d069d6a498b7cbf482da.

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -4,6 +4,5 @@ public class ArithmeticException {
     public static void main(String[] args) {
         System.out.println("This program is working");
         System.out.println("Added fourth line");
-        System.out.println("Added tenth line");
     }
 }
