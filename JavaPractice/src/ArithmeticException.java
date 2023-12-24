ac2077f72756416b036e4d492ef0c1ac5e11ef04
venkat <naresh3456@gmail.com>
2023-12-24 13:54:37 -0500

    Revert "Added  Twelth line"
    This reverts commit e563e20a

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -4,7 +4,5 @@ public class ArithmeticException {
     public static void main(String[] args) {
         System.out.println("This program is working");
         System.out.println("Added fourth line");
-        System.out.println("Added Twelth line");
-        System.out.println("Added Thirteen line");
     }
 }
