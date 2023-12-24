b4ca36f2266061bd787f63e177677b7fb225f2fd
venkat <naresh3456@gmail.com>
2023-12-24 14:11:13 -0500

    Added  seventeen line

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -1,8 +1,8 @@
 public class ArithmeticException {
 
-
     public static void main(String[] args) {
         System.out.println("This program is working");
         System.out.println("This sixteen is working");
+        System.out.println("This seventeen is working");
     }
 }
