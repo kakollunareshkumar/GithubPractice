2d4c3e37a2afcb32c54f2900171b1ba601a6672b
venkat <naresh3456@gmail.com>
2023-12-24 13:19:44 -0500

    Added fourth line

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -3,5 +3,6 @@ public class ArithmeticException {
 
     public static void main(String[] args) {
         System.out.println("This program is working");
+        System.out.println("Added fourth line");
     }
 }
