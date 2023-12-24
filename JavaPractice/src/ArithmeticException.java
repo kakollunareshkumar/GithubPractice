3ef2fb016852f1ccf60de5b4e4f0fb4677bf2fc3
venkat <naresh3456@gmail.com>
2023-12-24 13:59:34 -0500

    Revert "Added fourth line"
    This reverts commit 2d4c3e37

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -3,8 +3,5 @@ public class ArithmeticException {
 
     public static void main(String[] args) {
         System.out.println("This program is working");
-        System.out.println("Added fourth line");
-        System.out.println("Added fourteen line");
-        System.out.println("Added fifteen line");
     }
 }
