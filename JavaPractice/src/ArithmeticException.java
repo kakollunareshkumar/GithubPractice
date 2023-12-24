e563e20ab4e64590baba461d6f2b48f30d46c106
venkat <naresh3456@gmail.com>
2023-12-24 13:51:37 -0500

    Added  Twelth line

--- JavaPractice/src/ArithmeticException.java
+++ JavaPractice/src/ArithmeticException.java
@@ -4,5 +4,6 @@ public class ArithmeticException {
     public static void main(String[] args) {
         System.out.println("This program is working");
         System.out.println("Added fourth line");
+        System.out.println("Added Twelth line");
     }
 }
